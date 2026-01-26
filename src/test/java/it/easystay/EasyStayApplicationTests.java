cdb2879ffae154bf43f34f96070ba7c6a67a0839
giuseppe.varisano <giuseppe.varisano@hrm.group>
2026-01-26 09:18:32 +0100

    feat: ci/cd setup con github actions

--- src/test/java/it/easystay/EasyStayApplicationTests.java
+++ src/test/java/it/easystay/EasyStayApplicationTests.java
@@ -2,8 +2,10 @@ package it.easystay;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("h2") // <--- Questo dice a Spring: "Ignora MySQL, usa application-test.properties"
 class EasyStayApplicationTests {
 
     @Test
--- src/test/java/it/easystay/service/PrenotazioneServiceE2E.java
+++ src/test/java/it/easystay/service/PrenotazioneServiceE2E.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@ActiveProfiles("h2") // <--- Questo dice a Spring: "Ignora MySQL, usa application-test.properties"
 @AutoConfigureMockMvc
 @Transactional
 class PrenotazioneServiceE2ETest {
